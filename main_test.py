e043eeb3f6a2920a176f26748b92cdf3a784b3c3
Florent Clairambault <florent@clairambault.fr>
2022-06-21 12:54:31 +0200

    Adding a dirty test

---
+++ main_test.py
@@ -0,0 +1,10 @@
+"""Testing the app"""
+
+import os
+
+class TestMain:
+    """Testing the app"""
+
+    def test_main(self):
+        """Testing the app"""
+        os.system('./main.py -U postgres')
